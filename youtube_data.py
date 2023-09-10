310a4770700d4f42ec2c960d4c7e9a9103a40925
aadithya1205 <71657452+aadithya1205@users.noreply.github.com>
2023-09-10 23:41:47 +0530

    Update youtube_data.py

--- youtube_data.py
+++ youtube_data.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Positive011205?",
+  password="**PASSWORD**",
   database="youtube_data"
 )
 mycursor=mydb.cursor()
